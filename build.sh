4bdb120d8c12f880d95ef7f25107d28cb709531e
Aswathi Sunil <aswathisunil238@gmail.com>
2026-09-16 10:43:48 +0530

    Fix Render admin user creation

--- build.sh
+++ build.sh
@@ -1,6 +1,43 @@
+
 #!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
+
 python manage.py migrate
+
+python manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get('ADMIN_USERNAME')
+password = os.environ.get('ADMIN_PASSWORD')
+email = os.environ.get('ADMIN_EMAIL', '')
+
+if username and password:
+    user, created = User.objects.get_or_create(
+        username=username,
+        defaults={
+            'email': email,
+            'is_staff': True,
+            'is_superuser': True,
+            'is_active': True,
+        }
+    )
+
+    user.email = email
+    user.is_staff = True
+    user.is_superuser = True
+    user.is_active = True
+    user.set_password(password)
+    user.save()
+
+    print(f'Admin user {username} {"created" if created else "updated"} successfully.')
+else:
+    print('ADMIN_USERNAME or ADMIN_PASSWORD is not configured. Skipping admin creation.')
+"
+
